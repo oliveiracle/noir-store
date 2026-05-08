ddb88b4d9d37b25f6188a541a26094ad194a586d
Cleino Frank <cleinofrank@gmail.com>
2026-05-08 23:49:01 +0100

    Add to bag functionality using session storage

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 
 def view_bag(request):
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity', 1))
+    redirect_url = request.POST.get('redirect_url', '/products/')
+
+    bag = request.session.get('bag', {})
+    key = str(item_id)
+
+    if key in bag:
+        bag[key] += quantity
+    else:
+        bag[key] = quantity
+
+    request.session['bag'] = bag
+    messages.success(request, f'"{product.name}" added to your bag.')
+    return redirect(redirect_url)
