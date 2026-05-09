863e1ba5b12d2c9b1f179f3ac67174bf93022ff1
Cleino Frank <cleinofrank@gmail.com>
2026-05-09 23:21:29 +0100

    Add update quantity and remove item functionality to bag

--- bag/views.py
+++ bag/views.py
@@ -23,3 +23,26 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     messages.success(request, f'"{product.name}" added to your bag.')
     return redirect(redirect_url)
+
+
+def update_bag(request, item_id):
+    quantity = int(request.POST.get('quantity', 1))
+    bag = request.session.get('bag', {})
+    key = str(item_id)
+
+    if quantity > 0:
+        bag[key] = quantity
+    else:
+        bag.pop(key, None)
+
+    request.session['bag'] = bag
+    return redirect('view_bag')
+
+
+def remove_from_bag(request, item_id):
+    product = get_object_or_404(Product, pk=item_id)
+    bag = request.session.get('bag', {})
+    bag.pop(str(item_id), None)
+    request.session['bag'] = bag
+    messages.success(request, f'"{product.name}" removed from your bag.')
+    return redirect('view_bag')
