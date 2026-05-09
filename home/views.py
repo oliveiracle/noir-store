35341f68896aa93b2053760b4f6ea6f47af5797e
Cleino Frank <cleinofrank@gmail.com>
2026-05-09 23:56:17 +0100

    Add mouse-driven collection strip to home page Horizontal filmstrip of all products pans with the mouse using lerp animation for a smooth, weighted feel.

--- home/views.py
+++ home/views.py
@@ -4,5 +4,6 @@ from products.models import Product
 
 def index(request):
     new_products = Product.objects.filter(is_new=True).order_by('?')[:4]
-    context = {'new_products': new_products}
+    strip_products = Product.objects.filter(image__isnull=False).exclude(image='')
+    context = {'new_products': new_products, 'strip_products': strip_products}
     return render(request, 'home/index.html', context)
