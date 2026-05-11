4fd74176570eaecace1e1afb4a08fb4f063c0afd
Cleino Frank <cleinofrank@gmail.com>
2026-05-11 19:15:43 +0100

    Add FAQ, Shipping, Returns and Contact static pages

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('faq/', views.faq, name='faq'),
+    path('shipping/', views.shipping, name='shipping'),
+    path('returns/', views.returns, name='returns'),
+    path('contact/', views.contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,19 @@ def index(request):
     strip_products = Product.objects.filter(image__isnull=False).exclude(image='')
     context = {'new_products': new_products, 'strip_products': strip_products}
     return render(request, 'home/index.html', context)
+
+
+def faq(request):
+    return render(request, 'home/faq.html')
+
+
+def shipping(request):
+    return render(request, 'home/shipping.html')
+
+
+def returns(request):
+    return render(request, 'home/returns.html')
+
+
+def contact(request):
+    return render(request, 'home/contact.html')
