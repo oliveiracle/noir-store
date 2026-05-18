4f101880c041c7bd579679ef5939bdbc3a96f349
Cleino Frank <cleinofrank@gmail.com>
2026-05-18 22:58:59 +0100

    Pre-fill checkout form for authenticated users (US34)
    Logged-in users with a saved profile have their delivery details and
    email pre-filled at checkout so they are never asked for information
    the site already has.

--- checkout/views.py
+++ checkout/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.conf import settings
 from products.models import Product
 from bag.contexts import bag_contents
+from profiles.models import UserProfile
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 
@@ -61,7 +62,24 @@ def checkout(request):
                 'There was an error with your form. Please check your details.'
             )
     else:
-        order_form = OrderForm()
+        initial = {}
+        if request.user.is_authenticated:
+            try:
+                profile = request.user.profile
+                initial = {
+                    'full_name': request.user.get_full_name() or request.user.username,
+                    'email': request.user.email,
+                    'phone_number': profile.phone_number,
+                    'street_address1': profile.street_address1,
+                    'street_address2': profile.street_address2,
+                    'town_or_city': profile.town_or_city,
+                    'postcode': profile.postcode,
+                    'country': profile.country,
+                    'county': profile.county,
+                }
+            except UserProfile.DoesNotExist:
+                pass
+        order_form = OrderForm(initial=initial)
 
     context = {
         'order_form': order_form,
